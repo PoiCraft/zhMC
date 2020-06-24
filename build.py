70ce650859a6bd9948ebb06e60448602ed93bfa7
gggxbbb <2331490629@qq.com>
2020-06-24 21:55:44 +0800

    suppost 1.16

--- build.py
+++ build.py
@@ -1,3 +1,17 @@
+import json
+import datetime
+import pytz
+
+info = json.loads(open('manifest.json').read())
+version = '.'.join([str(v) for v in info['header']['version']])
+time = datetime.datetime.now(pytz.timezone('UTC')).strftime('%Y-%m-%d %H:%M:%S')
+
+extra = [
+        'Github: https://github.com/PoiCraft/zhMC',
+        f'Version: {version}',
+        f'BuildAt: {time} UTC'
+        ]
+
 source = open('data/zh_CN.lang').read()
 diff = open('data/zh_PC.diff.lang').read()
 
@@ -10,11 +24,17 @@ def loadLang(lang_str):
         if (lang_w[0] == '#' or lang_w[0] == ' '):
             continue
         lang_d = lang_w.split('=')
+        if len(lang_d) < 2:
+            continue
+        if not '.' in lang_d[0]:
+            continue
         lang_map[lang_d[0]] = lang_d[1]
     return lang_map
 
 def exportLang(lang_map):
     lang_list=[]
+    for v in extra:
+        lang_list.append('#'+v)
     for k in lang_map:
         lang_list.append(k+'='+lang_map[k])
     lang_str = '\n'.join(lang_list)
